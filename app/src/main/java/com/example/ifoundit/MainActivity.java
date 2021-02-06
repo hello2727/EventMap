fd50b65678b89e9acd35c9030014276633972122
hello2727 <jihye7369@gmail.com>
2021-02-06 17:09:06 +0900

    네이버맵 인증 실패 처리

--- app/src/main/java/com/example/ifoundit/MainActivity.java
+++ app/src/main/java/com/example/ifoundit/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ifoundit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.naver.maps.map.NaverMapSdk;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //NaverMap 객체 얻어오기
+        getMapInstance();
 
+    }
+
+    void getMapInstance(){
+        //인증 실패 처리
+        NaverMapSdk.getInstance(this).setOnAuthFailedListener(new NaverMapSdk.OnAuthFailedListener() {
+            @Override
+            public void onAuthFailed(@NonNull NaverMapSdk.AuthFailedException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
