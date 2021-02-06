b00f1c9fb950feae3838ec6b01d0e027e2874852
hello2727 <jihye7369@gmail.com>
2021-02-06 16:49:51 +0900

    버전 업데이트 && 재정비

--- app/src/main/java/com/example/ifoundit/MainActivity.java
+++ app/src/main/java/com/example/ifoundit/MainActivity.java
@@ -1,20 +1,9 @@
 package com.example.ifoundit;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
-import com.naver.maps.map.LocationTrackingMode;
-import com.naver.maps.map.MapFragment;
-import com.naver.maps.map.NaverMap;
-import com.naver.maps.map.OnMapReadyCallback;
-import com.naver.maps.map.UiSettings;
-import com.naver.maps.map.overlay.LocationOverlay;
-import com.naver.maps.map.util.FusedLocationSource;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
-    private FusedLocationSource locationSource;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,42 +11,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
 
         //NaverMap 객체 얻어오기
-        FragmentManager fm = getSupportFragmentManager();
-        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
-        if (mapFragment == null){
-            mapFragment = MapFragment.newInstance();
-            fm.beginTransaction().add(R.id.map, mapFragment).commit();
-        }
-        mapFragment.getMapAsync(this);
 
-        //내장 위치 추적 기능 사용(위치 오버레이)
-        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
-    }
-
-    //최적의 위치를 반환하는 구현체
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
-            return;
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-    @Override
-    public void onMapReady(@NonNull NaverMap naverMap) {
-        //네이버 지도 SDK에 위치를 제공하는 인터페이스
-        naverMap.setLocationSource(locationSource);
-
-        //위치 추적 모드
-        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
-
-        //객체 접근(위치 오버레이)
-        LocationOverlay locationOverlay = naverMap.getLocationOverlay();
-        locationOverlay.setVisible(true);
-        //객체 접근(현위치 버튼)
-        UiSettings uiSettings = naverMap.getUiSettings();
-        uiSettings.setLocationButtonEnabled(true);
-
-        //원의 반경 지정
-        locationOverlay.setCircleRadius(100);
     }
 }
