03cd18f3578145daaffc290a3bee871d7b852eab
hello2727 <jihye7369@gmail.com>
2022-01-09 17:52:45 +0900

    viewbinding, databinding 종속항목 추가

---
+++ EventMap/app/src/main/java/com/example/android/eventmap/view/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.android.eventmap.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import com.naver.maps.map.OnMapReadyCallback
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * Created by Jihye Noh
+ * Date: 2022-01-09
+ */
+//@AndroidEntryPoint
+//class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+//
+//}
