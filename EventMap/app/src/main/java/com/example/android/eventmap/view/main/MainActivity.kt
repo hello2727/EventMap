eee12f6958f930a8c4a31525ca5571e1defb3f8f
hello2727 <jihye7369@gmail.com>
2022-01-13 23:14:43 +0900

    hilt annotation 추가

--- EventMap/app/src/main/java/com/example/android/eventmap/view/main/MainActivity.kt
+++ EventMap/app/src/main/java/com/example/android/eventmap/view/main/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.eventmap.databinding.ActivityMainBinding
 import com.example.android.eventmap.databinding.NavigationheaderSettingBinding
+import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,15 +15,17 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 //@AndroidEntryPoint
 //class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-//    private val binding : ActivityMainBinding by lazy {
+//    private val binding: ActivityMainBinding by lazy {
 //        ActivityMainBinding.inflate(layoutInflater)
 //    }
 //
-//    private val settingBinding : NavigationheaderSettingBinding by lazy {
-//        NavigationheaderSettingBinding.inflate(layoutInflater)
-//    }
+//    private val viewModel: MainViewModel by viewModels()
 //
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
+//    }
+//
+//    override fun onMapReady(p0: NaverMap) {
+//        TODO("Not yet implemented")
 //    }
 //}
--- EventMap/app/src/main/java/com/example/android/eventmap/view/main/MainViewModel.kt
+++ EventMap/app/src/main/java/com/example/android/eventmap/view/main/MainViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.android.eventmap.view.main
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 /**
  * Created by Jihye Noh
  * Date: 2022-01-10
  */
-class MainViewModel : ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor(
+
+) : ViewModel() {
 }
